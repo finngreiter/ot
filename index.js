5b2be29e2ded82186e58c43f8d0febc14e8e28c4
Finn Greiter <finn.greiter@gmail.com>
2019-06-23 15:51:22 -0700

    v1.0.1

--- index.js
+++ index.js
@@ -3,16 +3,22 @@ const exec = require('child_process').exec
 
 module.exports = () => {
   let commandsRaw = fs.readFileSync('otlist.json')
-  let commands = JSON.parse(commandsRaw).commands
-  console.log(commands)
-  commands.forEach((x) => {
-    exec(x, (err, stdout, stderr) => {
-      if(stdout !== null) {
-        console.log(stdout)
-      }
-      if(stderr !== null) {
-        console.log(stderr)
-      }
+  let parsedJSON = JSON.parse(commandsRaw)
+  let cmds = process.argv.slice(2)
+
+  function justDoIt(stuff) {
+    // console.log(commands)
+    stuff.forEach((x) => {
+      console.log(`Running \'${x}\'.`)
+      exec(x, (err, stdout, stderr) => {
+        if(stdout !== null) {
+          console.log(stdout)
+        }
+        if(stderr !== null) {
+          console.log(stderr)
+        }
+      })
     })
-  })
+  }
+  justDoIt(parsedJSON[cmds])
 }
